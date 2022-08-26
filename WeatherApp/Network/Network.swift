0361847f39608f31392979b2615dc97f3926bc15
ertugrulozvardar <ertugrul.ozvardar@obss.com.tr>
2022-08-26 17:54:32 +0300

    Network layer has been implemented

---
+++ WeatherApp/Network/Network.swift
@@ -0,0 +1,39 @@
+//
+//  Network.swift
+//  WeatherApp
+//
+//  Created by pc on 26.08.2022.
+//
+
+import Foundation
+
+struct Network {
+    private let urlSession = URLSession.shared
+    
+    func performRequest<T: Codable>(request: URLRequest, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        let task = urlSession.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                if let data = data {
+                    do {
+                        let model = try JSONDecoder().decode(T.self, from: data)
+                        completion(.success(model))
+                    } catch {
+                        completion(.failure(.decodingError))
+                    }
+                } else if error != nil {
+                    completion(.failure(.networkError))
+                } else {
+                    completion(.failure(.unknownError))
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+enum NetworkError: Error {
+    case decodingError
+    case networkError
+    case unknownError
+    case invalidUrlError
+}
