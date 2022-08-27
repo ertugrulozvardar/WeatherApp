dcdb75695abbb51478315364dccb16101fc5abc7
ertugrulozvardar <ertugrul.ozvardar@obss.com.tr>
2022-08-27 13:12:48 +0300

    Custom cells have been implemented for the Weather Condition Page's table view

---
+++ WeatherApp/Views/WeatherCondition/WeatherConditionTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherConditionTableViewCell.swift
+//  WeatherApp
+//
+//  Created by pc on 27.08.2022.
+//
+
+import UIKit
+
+class WeatherConditionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
